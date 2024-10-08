2db132af1eceed865ad602ca82bad3aa4657d17a
tbruyer-imio <thomas.bruyer@imio.be>
2024-10-08 11:03:47 +0200

    Addind unit test function

--- src/imio/email/dms/tests/test_utils.py
+++ src/imio/email/dms/tests/test_utils.py
@@ -11,3 +11,24 @@ class TestUtils(unittest.TestCase):
         self.assertTupleEqual((True, (400, 400)), get_reduced_size((500, 500), 400))
         self.assertTupleEqual((True, (400, 333)), get_reduced_size((600, 500), 400))
         self.assertTupleEqual((True, (300, 400)), get_reduced_size((600, 800), 400))
+
+    def test_safe_unicode(self):
+        pass
+
+    def test_save_as_eml(self):
+        pass
+
+    def test_reception_date(self):
+        pass
+
+    def test_get_next_id(self):
+        pass
+
+    def test_get_reduced_size(self):
+        pass
+
+    def test_get_unique_name(self):
+        pass
+
+    def test_set_next_id(self):
+        pass
