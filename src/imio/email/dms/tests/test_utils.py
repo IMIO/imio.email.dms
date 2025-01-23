7ea058a31e56ac854d57c664dd331c3795e28c1e
Stephan Geulette <s.geulette@imio.be>
2025-01-23 14:53:24 +0100

    Corrected tests

--- src/imio/email/dms/tests/test_utils.py
+++ src/imio/email/dms/tests/test_utils.py
@@ -7,7 +7,8 @@ import unittest
 
 class TestUtils(unittest.TestCase):
     def test_get_reduced_size(self):
-        self.assertTupleEqual((False, None), get_reduced_size((500, 500), 600))
-        self.assertTupleEqual((True, (400, 400)), get_reduced_size((500, 500), 400))
-        self.assertTupleEqual((True, (400, 333)), get_reduced_size((600, 500), 400))
-        self.assertTupleEqual((True, (300, 400)), get_reduced_size((600, 800), 400))
+        self.assertTupleEqual((False, (500, 500)), get_reduced_size((500, 500), (None, None)))
+        self.assertTupleEqual((False, (500, 500)), get_reduced_size((500, 500), (600, None)))
+        self.assertTupleEqual((True, (400, 400)), get_reduced_size((500, 500), (400, 450)))
+        self.assertTupleEqual((True, (400, 333)), get_reduced_size((600, 500), (400, None)))
+        self.assertTupleEqual((True, (300, 400)), get_reduced_size((600, 800), (None, 400)))
