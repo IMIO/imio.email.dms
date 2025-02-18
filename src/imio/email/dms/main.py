5d4fa64d0a730797d572497ebda540ca03f50bf5
Chris Adam <chris.adam@imio.be>
2025-02-18 16:20:26 +0100

    Adding more tests on main.py WIP

--- src/imio/email/dms/main.py
+++ src/imio/email/dms/main.py
@@ -37,12 +37,10 @@ from imio.email.dms.utils import get_reduced_size
 from imio.email.dms.utils import get_unique_name
 from imio.email.dms.utils import safe_text
 from imio.email.dms.utils import save_as_eml
-from imio.email.dms.utils import save_attachment  # noqa
 from imio.email.dms.utils import set_next_id
 from imio.email.parser import email_policy  # noqa
 from imio.email.parser.parser import Parser  # noqa
 from imio.email.parser.utils import stop  # noqa
-from imio.email.parser.utils import structure  # noqa
 from imio.pyutils.system import runCommand
 from io import BytesIO
 from PIL import Image
@@ -294,24 +292,6 @@ def resize_inline_images(mail_id, message, attachments):
             if not img_cid or img_cid not in cids:
                 continue
             cids[img_cid]["used"] = True
-            # if "width" in img_tag.attrs:
-            #     img_tag["width"] = cids[img_cid]["sz"][0]
-            #     changes.add(img_cid)
-            # if "height" in img_tag.attrs:
-            #     img_tag["height"] = cids[img_cid]["sz"][1]
-            #     changes.add(img_cid)
-            # style = img_tag.get("style", "")
-            # if style:
-            #     if "width:" in style:
-            #         style = re.sub(r"width:\s*\d+(px|%)", f"width: {cids[img_cid]['sz'][0]}px", style)
-            #         img_tag["style"] = style
-            #         changes.add(img_cid)
-            #     if "height:" in style:
-            #         style = re.sub(r"height:\s*\d+(px|%)", f"height: {cids[img_cid]['sz'][1]}px", style)
-            #         img_tag["style"] = style
-            #         changes.add(img_cid)
-            # current_width = img_tag.get("width", "auto")
-            # img_tag["style"] = f"max-width: 100%; width: {current_width}; height: auto;"
             style_dic = {}
             if "style" in img_tag.attrs:
                 styles = img_tag["style"].split(";")
@@ -373,7 +353,8 @@ def send_to_ws(config, headers, main_file_path, attachments, mail_id):
     tar_path = Path("/tmp") / "{}.tar".format(external_id)
     with tarfile.open(str(tar_path), "w") as tar:
         # 1) email pdf printout or eml file
-        mf_contents = Path(main_file_path).open("rb").read()
+        with Path(main_file_path).open("rb") as f:
+            mf_contents = f.read()
         basename, ext = os.path.splitext(main_file_path)
         mf_info = tarfile.TarInfo(name="email{}".format(ext))
         mf_info.size = len(mf_contents)
@@ -666,6 +647,7 @@ def process_mails():
         logger.info("Treated no email.")
     handler.disconnect()
     lock.close()
+    sys.exit()
 
 
 def clean_mails():
@@ -742,6 +724,7 @@ def clean_mails():
         )
     )
     Notify(None, config, None).result("Result of clean_mails", "\n".join(out))
+    sys.exit()
 
 
 class Notify:
--- src/imio/email/dms/tests/test_utils.py
+++ src/imio/email/dms/tests/test_utils.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 
+from imio.email.dms import dev_mode
+from imio.email.dms.utils import get_next_id
 from imio.email.dms.utils import get_reduced_size
+from imio.email.dms.utils import set_next_id
+from unittest.mock import patch
 
+import configparser
 import unittest
 
 
@@ -12,3 +17,23 @@ class TestUtils(unittest.TestCase):
         self.assertTupleEqual((True, (400, 400)), get_reduced_size((500, 500), (400, 450)))
         self.assertTupleEqual((True, (400, 333)), get_reduced_size((600, 500), (400, None)))
         self.assertTupleEqual((True, (300, 400)), get_reduced_size((600, 800), (None, 400)))
+
+    def test_next_id(self):
+        config = configparser.ConfigParser()
+        config.read("../../config.ini.dev")
+
+        devinfos = {"nid": None}
+        with patch("imio.email.dms.dev_mode", True):
+            set_next_id(config, 0)
+            self.assertTupleEqual((1, "01Z9996"), get_next_id(config, devinfos))
+            self.assertTupleEqual((2, "01Z9996"), get_next_id(config, devinfos))
+            self.assertTupleEqual((3, "01Z9996"), get_next_id(config, devinfos))
+
+        # devinfos = {"nid": None}
+        # with patch("imio.email.dms.dev_mode", False):
+        #     set_next_id(config, 0)
+        #     self.assertTupleEqual((1, "01Z9996"), get_next_id(config, devinfos))
+        #     self.assertTupleEqual((1, "01Z9996"), get_next_id(config, devinfos))
+        #     self.assertTupleEqual((1, "01Z9996"), get_next_id(config, devinfos))
+
+        # FIXME check why patch doesn't work here
